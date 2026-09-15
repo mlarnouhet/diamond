40e0cb7c18bc6bc8f317b8dd7c4682e0673da68f
mlarnouhet <mlarnouhet@gmail.com>
2026-09-15 17:27:40 +0200

    add dataset preallocation

--- dataset.py
+++ dataset.py
@@ -5,11 +5,15 @@ import torch
 
 class Dataset():
     def __init__(self, args: Namespace):
-        self.data: Dict[str, torch.Tensor] = {
-            "observation": torch.empty(0, 3, 64, 64, device="cpu"),
-            "action": torch.empty(0, dtype=torch.int64, device="cpu"),
-            "reward": torch.empty(0, device="cpu"),
-            "end": torch.empty(0, dtype=torch.bool, device="cpu"),
+
+        self.capacity = 100_000
+        self.size = 0
+
+        self.data: Dict[str, torch.Tensor | List[int]] = {
+            "observation": torch.empty(self.capacity, 3, 64, 64, device="cpu"),
+            "action": torch.empty(self.capacity, dtype=torch.int64, device="cpu"),
+            "reward": torch.empty(self.capacity, device="cpu"),
+            "end": torch.empty(self.capacity, dtype=torch.bool, device="cpu"),
             "trajectory_index": [0]
         }
         self.batch_size = args.batch_size
@@ -30,20 +34,37 @@ class Dataset():
         return tensor_batch
 
     def add(self, trajectory: List[Dict[str, torch.Tensor]], end_of_traj: bool = False):
+        if self.size >= self.capacity:
+            raise OverflowError("Replay buffer capacity exceeded")
+
         trajectory = self._preprocess(trajectory)
-        self.data.update({k: torch.concatenate([self.data[k], trajectory[k].unsqueeze(0)], dim=0) for k in self.keys})
+        for key in self.keys:
+            self.data[key][self.size] = trajectory[key]
+        self.size += 1
+
         if end_of_traj:
-            self.data["trajectory_index"].append(len(self.data["observation"]))
+            self.data["trajectory_index"].append(self.size)
 
     def save_dataset(self, checkpoint_dir: str = None):
         assert checkpoint_dir is not None, "Dataset checkpoint_dir is None !"
-        data_to_save = self.data.copy()
-        data_to_save["observation"] = ((data_to_save["observation"] + 1) * 127.5).to(torch.uint8)
+        data_to_save = {
+            key: self.data[key][:self.size].clone()
+            for key in self.keys if key != "observation"
+        }
+        observations = self.data["observation"][:self.size].clone()
+        data_to_save["observation"] = ((observations + 1) * 127.5).to(torch.uint8)
+        data_to_save["trajectory_index"] = self.data["trajectory_index"]
         torch.save(data_to_save, checkpoint_dir)
 
     def load_dataset(self, checkpoint_dir):
-        self.data = torch.load(checkpoint_dir)
-        self.data["observation"] = (self.data["observation"].to(torch.float32) / 127.5 - 1)
+        data = torch.load(checkpoint_dir)
+        size = len(data["observation"])
+
+        for key in self.keys:
+            self.data[key][:size] = data[key]
+        self.data["observation"][:size] = self.data["observation"][:size].float() / 127.5 - 1
+        self.data["trajectory_index"] = data["trajectory_index"]
+        self.size = size
 
     def _preprocess(self, trajectory: List[Dict[str, torch.Tensor]]) -> List[Dict[str, torch.Tensor]]:
         observation = trajectory["observation"].squeeze(0).cpu()
@@ -56,5 +77,4 @@ class Dataset():
             "reward": reward,
             "end": end
         }
-
 
